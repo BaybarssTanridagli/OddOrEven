06ca1ffd6267c0f543fcc954b7ae0ef746e3caf5
Baybars <baybarsstanridagli@gmail.com>
2025-01-13 15:37:28 +0300

    OddOrEven

---
+++ OddOrEven/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OddOrEven
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int number;
+            Console.WriteLine("Please Enter The Number: ");
+            number = Convert.ToInt32(Console.ReadLine());
+            if (number % 2 == 0)
+            {
+                Console.WriteLine("This Number is Odd");
+            }
+            else
+            {
+                Console.WriteLine("This Number is Even");
+            }
+            Console.ReadKey();
+        }
+    }
+}
